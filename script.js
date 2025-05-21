390db55bb3d9069eb957f5629c77e799aed25e0e
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-21 12:11:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 let id=document.getElementById("loaad");
 window.addEventListener("load",()=>{
-	id.textContent="DOM load sucess";
+	id.textContent="DOM load success";
 })
